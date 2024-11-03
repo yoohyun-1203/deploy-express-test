04f97ae13e44a3ea2dd5be639ae6f492ca39b43e
yoohyun-1203 <yoohyun9448@gmail.com>
2024-11-03 16:40:00 +0900

    express v2

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ import User from "../schemas/user.js";
 
 const main = (req, res) => {
   res.set({"Content-Type" : "text/html; charset=utf-8"});
-  res.send("<h1>Welcome Express!</h1>")
+  res.send("<h1>Welcome Express! v2😎</h1>")
 }
 
 const test = async (req, res) => {
